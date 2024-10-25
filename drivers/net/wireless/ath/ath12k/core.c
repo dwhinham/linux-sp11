63738575ac09953c529af8b8d1528ba4c573f5d2
Jiajie Chen <c@jia.je>
2024-10-25 13:38:57 +0800

    HACK: disable rfkill of ath12k_pci

--- drivers/net/wireless/ath/ath12k/core.c
+++ drivers/net/wireless/ath/ath12k/core.c
@@ -80,6 +80,8 @@ static int ath12k_core_rfkill_config(struct ath12k_base *ab)
 	if (ath12k_acpi_get_disable_rfkill(ab))
 		return 0;
 
+	return 0;
+
 	for (i = 0; i < ab->num_radios; i++) {
 		ar = ab->pdevs[i].ar;
 
